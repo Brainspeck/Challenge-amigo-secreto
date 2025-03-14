a2ad9a38d20b59925c0acda4b757a8d9a50c2f49
Brainspeck <erikmaloschuessler@gmail.com>
2025-03-13 20:28:10 -0600

    se implementan  las funcionalidades basicas del proyecto

---
+++ README.md
@@ -0,0 +1,23 @@
+# Amigo Secreto
+
+Este es un pequeño proyecto hecho en JavaScript que permite agregar nombres a una lista y sortear un "amigo secreto" al azar. Fue hecho para el curso Alura ONE.
+
+## Características
+- Agregar nombres a una lista.
+- Mostrar los nombres agregados.
+- Seleccionar un nombre aleatorio como "amigo secreto".
+
+## Uso
+1. Ingresa un nombre en el campo de texto y presiona "Añadir".
+2. Los nombres se agregarán a la lista mostrada en pantalla.
+3. Presiona "Sortear amigo" para seleccionar un nombre al azar.
+4. Si la lista está vacía, se mostrará una alerta indicando que debes agregar nombres primero.
+
+## Tecnologías utilizadas
+- HTML
+- CSS
+- JavaScript
+
+## Autor
+Erik Malo Schuessler
+
--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo () {
+    let valorInput = document.getElementById("amigo").value;
+    if (!valorInput){
+        alert("Por favor, inserte un nombre.");
+    }
+
+    else {
+        amigos.push(valorInput);
+        crearLista()
+        document.getElementById("amigo").value = '';
+        console.log(amigos);
+    };
+    
+};
+
+function crearLista (){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i=0; i < amigos.length; i++){
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`
+    };
+};
+
+function sortearAmigo () {
+    if (amigos.length === 0) {
+        alert(`Debe agregar amigos a la lista primero`);
+        return;
+    }
+    let indiceRandom = Math.floor(Math.random() * amigos.length);
+    let amigoSeleccionado = amigos[indiceRandom];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSeleccionado}`;
+}
